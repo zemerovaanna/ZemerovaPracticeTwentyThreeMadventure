8faa9f0a1fa1f1c902b8a55addfacfc1ce546b89
Anna <123442002+zemerovaanna@users.noreply.github.com>
2023-11-07 16:52:29 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/madventurezemerova/MainActivity.kt
+++ app/src/main/java/com/example/madventurezemerova/MainActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
